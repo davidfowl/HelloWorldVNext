a9f5b0bfa89ad6c12e55432207d95f6c39b96aff
David Fowler <davidfowl@gmail.com>
2014-05-26 16:30:32 -0700

    Added basic mvc example

---
+++ src/hellomvc/Startup.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+public class Startup
+{
+    public void Configure(IBuilder app)
+    {
+        app.UseServices(services =>
+        {
+            services.AddMvc();
+        });
+
+        app.Use(async (context, next) => 
+        {
+            Console.WriteLine(context.Request.Path);
+
+            try
+            {
+                await next();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        });
+
+        app.UseMvc();
+    }
+}
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
